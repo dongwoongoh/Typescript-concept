f635fbcb9eacf6139335421d92f7577948c43d91
maaaaaaaaad <integral5137@gmail.com>
2024-02-05 20:46:23 +0900

    setup

---
+++ src/main/java/com/demo/RooseveltApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RooseveltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RooseveltApplication.class, args);
+	}
+
+}
