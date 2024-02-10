08eb4612fcdda7f4e6b5a27d4d2630e73919e39b
maaaaaaaaad <integral5137@gmail.com>
2024-02-10 20:40:45 +0900

    e

--- src/test/java/com/demo/repositories/character/CharacterMemoryRepositoryTest.java
+++ src/test/java/com/demo/repositories/character/CharacterMemoryRepositoryTest.java
@@ -9,13 +9,21 @@ import org.junit.jupiter.api.Test;
 
 public class CharacterMemoryRepositoryTest {
     private static final CharacterRepository characterRepository = new CharacterMemoryRepository();
+    private static final Long id = 1L;
 
     @Test()
     @DisplayName("should save a new character")
     void save() {
-        final Character character = new Character(1L);
+        final Character character = new Character(id);
         characterRepository.save(character);
         final Character findCharacter = characterRepository.find(character.getId());
         Assertions.assertThat(character.getId()).isEqualTo(findCharacter.getId());
+    }
+
+    @Test()
+    @DisplayName("should success find a character")
+    void find() {
+        final Character character = characterRepository.find(id);
+        Assertions.assertThat(character.getId()).isEqualTo(id);
     }
 }
